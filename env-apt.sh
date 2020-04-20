4c04255f9c1ab90463c4166b5f46708ccb26d54e
Damian Facchini <facchinidm@gmail.com>
2020-04-20 11:58:29 -0300

    Update apt-get to apt

--- env-apt.sh
+++ env-apt.sh
@@ -10,44 +10,44 @@ fi
 sudo apt-get update
 
 # Paquetes Herramientas
-sudo apt-get -y install aptitude 
-sudo apt-get -y install vim 
-sudo apt-get -y install emacs 
-sudo apt-get -y install nano 
-sudo apt-get -y install strace 
-sudo apt-get -y install ssh 
-sudo apt-get -y install curl 
-sudo apt-get -y install htop 
-sudo apt-get -y install tree 
-sudo apt-get -y install wget 
-sudo apt-get -y install terminator 
-sudo apt-get -y install xclip 
-sudo apt-get -y install bless 
-sudo apt-get -y install neofetch
-sudo apt-get -y install zsh
+sudo apt -y install aptitude 
+sudo apt -y install vim 
+sudo apt -y install emacs 
+sudo apt -y install nano 
+sudo apt -y install strace 
+sudo apt -y install ssh 
+sudo apt -y install curl 
+sudo apt -y install htop 
+sudo apt -y install tree 
+sudo apt -y install wget 
+sudo apt -y install terminator 
+sudo apt -y install xclip 
+sudo apt -y install bless 
+sudo apt -y install neofetch
+sudo apt -y install zsh
 
 # Lenguajes de Programacion
-sudo apt-get -y install build-essential 
-sudo apt-get -y install autotools-dev 
-sudo apt-get -y install gcc 
-sudo apt-get -y install gdb 
-sudo apt-get -y install g++ 
-sudo apt-get -y install python3 
-sudo apt-get -y install python3-pip 
-sudo apt-get -y install openjdk-8-jdk 
-sudo apt-get -y install libcunit1-dev 
-sudo apt-get -y install libcunit1 
-sudo apt-get -y install libevent1-dev 
-sudo apt-get -y install make 
-sudo apt-get -y install cmake
+sudo apt -y install build-essential 
+sudo apt -y install autotools-dev 
+sudo apt -y install gcc 
+sudo apt -y install gdb 
+sudo apt -y install g++ 
+sudo apt -y install python3 
+sudo apt -y install python3-pip 
+sudo apt -y install openjdk-8-jdk 
+sudo apt -y install libcunit1-dev 
+sudo apt -y install libcunit1 
+sudo apt -y install libevent1-dev 
+sudo apt -y install make 
+sudo apt -y install cmake
 
 # DEV Utils
-sudo apt-get -y install valgrind 
-sudo apt-get -y install meld 
+sudo apt -y install valgrind 
+sudo apt -y install meld 
 
 # Docker
-sudo apt-get -y install docker 
-sudo apt-get -y install docker-compose 
+sudo apt -y install docker 
+sudo apt -y install docker-compose 
 
 # Oh My ZSH
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
